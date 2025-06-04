fb8ffd9b318a70744825b6145da820e5e4780a5b
Daniel Bratto <danbratto@hotmail.com>
2025-06-03 20:34:05 -0400

    Sand doesn't fall to the sides right. But they do fall.

--- main.py
+++ main.py
@@ -1,34 +1,140 @@
 import tkinter as tk
 
-sands = {}
+print('Loading Game.')
+grid = list()
+for i in range(160):
+    grid.append([])
+    for j in range(160):
+        grid[i].append(0)
+print('Load Done.')
+    
+# if grid[x][y - 1] == 0:
+#     grid[x][y] = 0
+#     grid[x][y] = 1
 
-def place_sand(e):
-    global sands
-    x = e.x - (e.x % 10)
-    y = e.y - (e.y % 10)
-    sand_id = canvas.create_rectangle(x, y, x + 10, y + 10, fill = 'white')
-    sands[sand_id] = [x, y, x+10, y+10]
+# limits x: 800, y: 800
 
-def update_sand():
-    global sands
-    if len(sands) >= 1:
-        for sand_id in sands:
-            # canvas.coords(sand_id, (sands[sand_id][0], sands[sand_id][1] + 10), (sands[sand_id][2], sands[sand_id][3] + 10))
-            sands[sand_id][1] += 10
-            sands[sand_id][3] += 10
-            canvas.coords(sand_id, sands[sand_id][0], sands[sand_id][1], sands[sand_id][2], sands[sand_id][3])
 
-    canvas.after(500, update_sand)
+def draw_sand(x, y):
+    #.append(canvas.create_rectangle(x, y, x + 5, y + 5, fill = 'white'))
+    pass
+    
 
-root = tk.Tk()
-root.geometry('800x600')
-root.title('Falling Sands')
+def get_point(mouse, grid):
+    grid_x = int( (mouse.x - (mouse.x % 5)) / 5 )
+    grid_y = int( (mouse.y - (mouse.y % 5)) / 5 )
+    can_x = (mouse.x - (mouse.x % 5))
+    can_y = (mouse.y - (mouse.y % 5))
+    if grid[grid_x][grid_y] == 0:
+        grid[grid_x][grid_y] = canvas.create_rectangle(can_x, can_y, can_x+5, can_y+5, fill = 'white')
 
-canvas = tk.Canvas(root, width = 750, height = 500, bg = 'black')
-canvas.pack(anchor = tk.CENTER, expand = True)
+def update_sand(bunch_of_sand, grid):
+    for i in range(160):
+        for j in range(160):
+            if grid[i][j] == 1 and grid[i][j + 1] == 0:
+                grid[i][j] = 0
+                grid[i][j + 1] = 1
 
-canvas.bind('<Button>', place_sand)
 
-root.after(500, update_sand)
+window = tk.Tk()
+window.title('Falling Sand')
+window.geometry('900x900')
 
-root.mainloop()
+canvas = tk.Canvas(
+    window,
+    width = 800,
+    height = 800,
+    background = 'black'
+)
+
+canvas.pack(
+    anchor = tk.CENTER,
+    expand = True
+)
+
+canvas.bind('<B1-Motion>', lambda e: get_point(e, grid))
+canvas.bind('<Button-1>', lambda e: get_point(e, grid))
+
+window.mainloop()
+
+# def create_grid(n):
+#     grid = list()
+#     for i in range(n):
+#         grid.append([])
+#         for j in range(n):
+#             grid[i].append(0)
+#     return grid
+
+# x = None
+# y = None
+# grid = create_grid(100)
+
+# def place_sand(mouse):
+#     t0 = time.time_ns()
+#     global x, y, grid
+#     x = int((mouse.x - (mouse.x % 10)) / 10)
+#     y = int((mouse.y - (mouse.y % 10)) / 10)
+
+#     if x < 100 and y < 100 or x >= 0 and y >= 0:
+#         grid[y][x] = 1
+#     draw_sand()
+#     print(f'{(time.time_ns() - t0)/1000000} ms')
+
+# def draw_sand():
+#     global grid, x, y
+#     for row in range(100):
+#         for col in range(100):
+#             if grid[row][col] == 1:
+#                 canvas.create_rectangle(x*10, y*10, (x*10)+10, (y*10)+10, fill = 'white')
+
+
+# if __name__ == '__main__':
+#     root = tk.Tk()
+#     root.geometry('1000x1000')
+#     root.title('Falling Sand')
+
+#     canvas = tk.Canvas(
+#         root,
+#         width = 1000,
+#         height = 1000,
+#         bg = 'black')
+
+#     canvas.pack(
+#         anchor = tk.CENTER,
+#         expand = True)
+    
+#     canvas.bind('<B1-Motion>', place_sand)
+
+#     root.mainloop()
+
+
+# def place_sand(e):
+#     global sands
+#     x = e.x - (e.x % 10)
+#     y = e.y - (e.y % 10)
+#     sand_id = canvas.create_rectangle(x, y, x + 10, y + 10, fill = 'white')
+#     sands[sand_id] = [x, y, x+10, y+10]
+
+# def update_sand():
+#     global sands
+#     if len(sands) >= 1:
+#         for sand_id in sands:
+#             # canvas.coords(sand_id, (sands[sand_id][0], sands[sand_id][1] + 10), (sands[sand_id][2], sands[sand_id][3] + 10))
+#             sands[sand_id][1] += 10
+#             sands[sand_id][3] += 10
+#             canvas.coords(sand_id, sands[sand_id][0], sands[sand_id][1], sands[sand_id][2], sands[sand_id][3])
+
+#     canvas.after(500, update_sand)
+
+# root = tk.Tk()
+# root.geometry('800x600')
+# root.title('Falling Sands')
+
+# canvas = tk.Canvas(root, width = 750, height = 500, bg = 'black')
+# canvas.pack(anchor = tk.CENTER, expand = True)
+
+# canvas.bind('<Button>', place_sand)
+
+# root.after(500, update_sand)
+
+# root.mainloop()
---
+++ proto.py
@@ -0,0 +1,76 @@
+import tkinter as tk
+from random import randint
+
+width = 400
+height = 400
+box_size = 10
+
+row = int(height / box_size)
+col = int(width / box_size)
+
+window = tk.Tk()
+window.title('Falling Sand')
+window.geometry(f'{width + 5}x{height + 4}')
+
+canvas = tk.Canvas(
+    window,
+    width = width,
+    height = height,
+    background = 'black'
+)
+
+canvas.pack(
+    anchor = tk.CENTER,
+    expand = True
+)
+
+grid = list()
+another_grid = list()
+for i in range(row):
+    grid.append([])
+    another_grid.append([])
+    for j in range(col):
+        grid[i].append(0)
+        another_grid[i].append(0)
+
+def place_box(mouse, grid, box_size):
+    x = int((mouse.x - (mouse.x % box_size)) / box_size)
+    y = int((mouse.y - (mouse.y % box_size)) / box_size)
+    grid[y][x] = 1
+
+def draw_box(x, y, box_size):
+    canvas.create_rectangle( x * box_size, y * box_size, x * box_size + box_size, y * box_size + box_size, fill = 'white' )
+
+
+def update_grid(grid, another_grid, row, col, box_size):
+    # Clear canvas
+    canvas.delete('all')
+
+    # Search grid and draw square
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                draw_box(j, i, box_size)
+            if i < 39 and grid[i][j] == 1 and grid[i + 1][j] == 0:
+                another_grid[i+1][j] = 1
+                another_grid[i][j] = 0
+            elif j < 39 and j > 0 and i < 39 and grid[i][j] == 1 and grid[i + 1][j + 1] == 0 and grid[i + 1][j - 1] == 0:
+                coin = randint(0,1)
+                if coin == 1:
+                    another_grid[i + 1][j + 1] = 1
+                    another_grid[i][j] = 0
+                else:
+                    another_grid[i + 1][j - 1] = 1
+                    another_grid[i][j] = 0
+            grid[i][j] = another_grid[i][j]
+            
+
+    window.after(50, update_grid, grid, another_grid, row, col, box_size)
+
+canvas.bind('<B1-Motion>', lambda mouse: place_box(mouse, grid, box_size))
+canvas.bind('<Button-1>', lambda mouse: place_box(mouse, grid, box_size))
+
+# Start the sycle
+update_grid(grid, another_grid, row, col, box_size)
+
+window.mainloop()
